2ca39ae78ae90aa7b7e22b814e893c63449c9d40
Ali-consciousroad <ali_dindar@live.be>
2022-06-01 17:33:02 +0200

    Add comment - Simple Node Module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// File-based module
 // Use values imported from rectangles.js to calculate the area and perimeter of a rectangle
 const rectangle = require('./rectangle');
 var rect = require('./rectangle');
@@ -20,7 +21,7 @@ function solveRect(l,b) {
     });
 };
 
-// Call solveRect() function multiple times for different values 
+// Function call for different values 
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
